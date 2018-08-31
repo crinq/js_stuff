1cb14f9bfa67bad024d857a87a46f0dd419b523a
Nico Stute <crinq@users.noreply.github.com>
2018-08-31 02:26:37 +0200

    Update sandbox.js

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -64,6 +64,20 @@ class plot{
       
       print("<canvas id = '" + id + "_canvas' width = '" + width + "'' height='" + height + "'>not supported</canvas>");
    }
+  
+   line(x, y, x2, y2, color){
+      var canvas = $(this.id + "_canvas");
+      var ctx = canvas.getContext('2d');
+      
+      if(ctx){
+         ctx.beginPath();
+         ctx.strokeStyle=color;
+         ctx.moveTo(x, y);
+         ctx.lineTo(x2, y2);
+         ctx.stroke();
+         ctx.closePath();
+      }
+   }
    
    plot(x, y, color){
       if(x < this.x_min || x > this.x_max || y < this.y_min || y > this.y_max){
