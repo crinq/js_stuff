5755fcc181ea6d4618035f48d47f4165aaf295b7
crinq <crinq@crinq.de>
2019-05-05 23:07:06 +0200

    motsim

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -87,3 +87,144 @@ class plot{
       }
    }
 }
+
+class motor{
+   constructor(){
+      // config
+      this.r = 1;
+      this.ld = 0.0005;
+      this.lq = 0.0005;
+      this.psi = 0.05;
+      this.polecount = 2;
+      this.j = 0.000025;
+      this.period = 1.0 / 5000.0;
+
+      this.load = 0.0;
+      this.damping = 0.1;
+      this.friction = 0.1;
+
+      // state
+      this.id = 0.0;
+      this.iq = 0.0;
+
+      this.pos = 0.0;
+      this.vel = 0.0;
+
+      // derived state
+      this.e_vel = 0.0;
+      this.e_pos = 0.0;
+      this.m_pos = 0.0;
+      this.e_torque = 0.0;
+      this.m_torque = 0.0;
+      this.acc = 0.0;
+      this.psi_d = 0.0;
+      this.psi_q = 0.0;
+      this.ind_d = 0.0;
+      this.ind_q = 0.0;
+   }
+
+   pmsm(ud, uq){
+      this.elec(ud, uq);
+
+      this.e_torque = 3.0 / 2.0 * this.polecount * (this.psi * this.iq + (this.ld - this.lq) * this.id * this.iq);
+      this.mech();
+   }
+
+   acim(ud, uq){
+      this.elec(ud, uq);
+
+      this.e_torque = 3.0 / 2.0 * this.polecount * this.psi_d * this.iq; // TODO check this
+      this.mech();   
+   }
+
+   elec(ud, uq){
+      this.psi_d = this.ld * this.id + this.psi;
+      this.psi_q = this.lq * this.iq;
+      
+      this.ind_d = this.psi_q * this.e_vel;
+      this.ind_q = this.psi_d * this.e_vel;
+      
+      this.id += (ud - this.r * this.id + this.ind_d) / this.ld * this.period;
+      this.iq += (uq - this.r * this.iq - this.ind_q) / this.lq * this.period;
+   }
+
+   mech(){
+      this.m_torque = this.e_torque - this.load - this.damping * this.vel - this.friction * Math.sign(this.vel);
+      this.m_pos += this.vel * period;
+      this.m_pos = (this.m_pos + Math.PI) % (2.0 * Math.PI) - Math.PI;
+      this.e_pos = (this.m_pos * this.polecount + Math.PI) % (2.0 * Math.PI) - Math.PI;
+      this.vel += this.acc * period;
+      this.e_vel = this.vel * this.polecount;
+      this.acc = this.m_torque / this.j;
+   }
+}
+
+class pid{
+   constructor(){
+      // config
+      this.p = 1.0;
+      this.i = 1.0;
+      this.ff0 = 0.0;
+      this.ff1 = 0.0;
+      this.period = 1.0 / 5000.0;
+      this.max_output = 1.0;
+      this.max_pi = 1.0;
+
+      // state
+      this.sum = 0.0;
+      this.old_cmd = 0.0;
+
+      // derived state
+      this.cmd = 0.0;
+      this.fb = 0.0;
+      this.error = 0.0;
+      this.sat = 0.0;
+      this.max_sum = 0.0;
+      this.min_sum = 0.0;
+      this.output = 0.0;
+
+   }
+
+   run(cmd, fb){
+      this.cmd = cmd;
+      this.fb = fb;
+      this.error = this.cmd - this.fb;
+
+
+      this.sum += this.error * this.i * this.period; // i
+      this.output = this.error * this.p; // p
+
+      // p clamp
+      if(this.output > this.max_output * this.max_pi){
+         this.output = this.max_output * this.max_pi;
+      }
+      if(this.output < -this.max_output * this.max_pi){
+         this.output = -this.max_output * this.max_pi;
+      }
+
+      // i clamp
+      this.max_sum = this.max_output * this.max_pi - this.output;
+      this.min_sum = -this.max_output * this.max_pi - this.output;
+
+      if(this.sum > this.max_sum){
+         this.sum = this.max_sum;
+      }
+      if(this.sum < this.min_sum){
+         this.sum = this.min_sum;
+      }
+
+      this.output += this.sum;
+
+      this.output += this.ff0 * this.cmd; // ff0
+      this.output += this.ff1 * (this.cmd - this.old_cmd); // ff1
+
+      // output clamp
+      if(this.output > this.max_output){
+         this.output = this.max_output;
+      }
+
+      if(this.output < -this.max_output){
+         this.output = -this.max_output;
+      }
+   }
+}
